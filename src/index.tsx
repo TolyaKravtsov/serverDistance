0fc30afabc38fdfd857797ad82272959d69656fe
tolyakravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2023-08-26 13:56:56 +0200

    install Tailwind and react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,28 @@
-import ReactDOM from "react-dom/client";
+import React from "react";
 
-const App = () => (
-    <h1>first component</h1>
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./input.css";
+
+import { App } from "./App";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      ),
+    },
+  ],
+  { basename: "/" },
 );
 
-const container = document.getElementById('root') as HTMLHtmlElement;
-
+const container = document.getElementById("root") as HTMLHtmlElement;
 const root = ReactDOM.createRoot(container);
-
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,39 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.(ts)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'ts-loader',
-                },
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx",".ts",".js"],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-          template: "./public/index.html",
-          filename: "index.html",
-      })
-    ]
-}
-
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        include: resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
